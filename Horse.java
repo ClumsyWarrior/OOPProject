b629f524e7efd78fb5080091596b74f7d59162f8
Rayyan Parkar <rayyanparkar09@gmail.com>
2024-04-20 15:33:04 +0100

    Created Horse Class and Constructor

--- Horse.java
+++ Horse.java
@@ -7,8 +7,12 @@
  */
 public class Horse
 {
-    //Fields of class Horse
-    
+    //Attributes of class Horse
+    private String horseName;
+    private char horseSymbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double horseConfidence;
     
       
     //Constructor of class Horse
@@ -17,7 +21,11 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
     }
     
     
