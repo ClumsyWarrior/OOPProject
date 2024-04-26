3c3cda4ba1351174543f18cc46e1c3f0b20087fe
Rayyan Parkar <95403391+ClumsyWarrior@users.noreply.github.com>
2024-04-26 20:27:07 +0100

    Add files via upload

---
+++ Horse.java
@@ -0,0 +1,84 @@
+package Part2;
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author Rayyan Parkar 
+ * @version 0.1
+ */
+public class Horse
+{
+    //Attributes of class Horse
+    private String horseName;
+    private char horseSymbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double horseConfidence;
+    
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        this.hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return this.horseConfidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return this.distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return this.horseName;
+    }
+    
+    public char getSymbol()
+    {
+        return this.horseSymbol;
+    }
+    
+    public void goBackToStart()
+    {
+        this.distanceTravelled = 0;
+    }
+    
+    public boolean hasFallen()
+    {
+        return this.hasFallen;
+    }
+
+    public void moveForward()
+    {
+        this.distanceTravelled++;
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        this.horseConfidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.horseSymbol = newSymbol;
+    }
+    
+}
